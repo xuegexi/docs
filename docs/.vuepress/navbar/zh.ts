7d91a9ab5c79a02b28f7f78b3f472e293bb8bdd9
Ivan Gao <ivan.gao@cnosdb.com>
2023-04-27 10:55:25 +0800

    update version tag.

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -1,4 +1,13 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
+    {
+        text: '版本',
+        link: 'latest' ,
+        children: [
+            { text: 'latest', link: '' },
+            { text: 'v2.3', link: 'v2.3/' },
+        ]
+    },
+
 ]);
